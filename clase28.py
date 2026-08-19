36dadbb7357e438c1357edc614723ae2961ec1b0
Sergio Daniel Zacarias <sergiodcarp@gmail.com>
2026-08-19 16:58:59 -0300

    manipulacion de arrays con Numpy

--- clase28.py
+++ clase28.py
@@ -37,3 +37,43 @@ import numpy as np
 # arr = np.array([0, np.pi/2, np.pi])
 # print("Seno:", np.sin(arr)) 
 # print("Coseno:", np.cos(arr)) 
+
+# reshape : cambiar la forma de un array
+# arr = np.arange(12) # Array de 0 a 11
+# print("Array original:\n", arr)
+# mat = arr.reshape(3, 4) # 3 filas, 4 columnas
+# print("Array con reshape (3x4):\n", mat)
+# Podemos usar -1 para que NumPy calcule automáticamente una dimensión
+# mat = arr.reshape(-1, 6) # NumPy ajusta la cantidad de filas
+# print(mat)
+
+# Concatenación de arrays
+# a = np.array([1, 2, 3])
+# b = np.array([4, 5, 6])
+# c = np.concatenate([a, b])
+# print("Concatenación:", c)
+
+# Apilamiento horizontal ( np.hstack )
+# a = np.array([[1, 2], [3, 4]])
+# b = np.array([[5, 6], [7, 8]])
+# c = np.hstack((a, b))
+# print("Apilamiento horizontal:\n", c)
+
+# Apilamiento vertical ( np.vstack )
+# c = np.vstack((a, b))
+# print("Apilamiento vertical:\n", c)
+
+# División de arrays ( split )
+# Podemos dividir un array en sub-arrays con np.split y funciones relacionadas
+# División en 1D
+# arr = np.arange(10)
+# dividido = np.split(arr, 2)
+# print("División en 2 partes:", dividido)
+
+# División en 2D
+mat = np.arange(16).reshape(4, 4)
+print("Matriz original:\n", mat)
+# Dividir en 2 bloques horizontales
+print("División vertical:\n", np.vsplit(mat, 2))
+# Dividir en 2 bloques verticales
+print("División horizontal:\n", np.hsplit(mat, 2))
